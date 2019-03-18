b6e20d6aad5fa092dbe47651290df62e5e778fe5
dubilka <47749922+dubilka@users.noreply.github.com>
2019-03-18 17:53:58 +0200

    Add files via upload

---
+++ character.gd
@@ -0,0 +1,16 @@
+extends Sprite
+var helth = 10
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_Button2_pressed(extra_arg_0):
+		helth += extra_arg_0
+		print (helth)
